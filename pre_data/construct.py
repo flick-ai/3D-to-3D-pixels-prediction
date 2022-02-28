80432700ac401b9de823b8f5ad6c7703353e5263
Flick <1904456330@qq.com>
2022-02-28 22:06:11 +0800

    注释语句

--- pre_data/construct.py
+++ pre_data/construct.py
@@ -3,14 +3,14 @@ import numpy as np
 import cv2
 import matplotlib.image as mpig
 import os
-from pre_data.NLM import NLM
-from pre_data.SSIM import SSIM
+from NLM import NLM
+from SSIM import SSIM
 
 
 def project():
     # 请使用者在此处添加你所使用的数据集的绝对路径
     path = "D:/Filez/DownLoad/大作业/OCTA/OCTA_6M_OCTA"
-    save_path = "pre_data/data"
+    save_path = "data"
     pack_img = np.arange(10001, 10002, 1)
     div_img = np.arange(1, 401, 1)
     for j in pack_img:
@@ -28,7 +28,7 @@ def project():
         projection = np.flip(projection, 0)
         # 保存原始投影图像
         cv2.imwrite(save_path + "/2D/" + str(j) + ".bmp", projection)
-        print(os.path.exists(path))
+        print(os.path.exists(save_path))
         projection_nlm = NLM(projection)
         # 保存NLM滤波后投影图像
         cv2.imwrite(save_path + "/2D_NLM/" + str(j) + ".bmp", projection_nlm)
